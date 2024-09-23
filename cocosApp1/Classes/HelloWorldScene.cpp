83725b288b641e3e377b58edbe9f2d625227e78c
usbport11 <51235513+usbport11@users.noreply.github.com>
2024-09-23 22:27:56 +0300

    Add files via upload

--- cocosApp1/Classes/HelloWorldScene.cpp
+++ cocosApp1/Classes/HelloWorldScene.cpp
@@ -7,13 +7,44 @@ Scene* HelloWorld::createScene() {
     return HelloWorld::create();
 }
 
-// Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename) {
     printf("Error while loading: %s\n", filename);
     printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
-// on "init" you need to initialize your instance
+bool HelloWorld::createAnimSpriteFromPlist(const std::string& fileName, const std::string& spriteName, const std::string& prefix, int count, float step) {
+    if (fileName.empty() || spriteName.empty() || prefix.empty() || count <= 0) {
+        return false;
+    }
+
+    auto spritecache = SpriteFrameCache::getInstance();
+    if (!spritecache) {
+        return false;
+    }
+
+    Vector<SpriteFrame*> animFrames;
+    spritecache->addSpriteFramesWithFile(fileName);
+    std::string key;
+    char buf[3];
+    for (int i = 0; i < count; i++) {
+        memset(buf, 0, 3);
+        itoa(i, buf, 10);
+        key = prefix + buf;
+        animFrames.pushBack(spritecache->getSpriteFrameByName(key));
+    }
+
+    cocos2d::Sprite* animSprite_fox = Sprite::createWithSpriteFrame(animFrames.front());
+    animSprite_fox->setName(spriteName);
+    animSprite_fox->getTexture()->setAliasTexParameters();
+    cocos2d::Animation* animation_fox = Animation::createWithSpriteFrames(animFrames, step);
+    cocos2d::Animate* animate_fox = Animate::create(animation_fox);
+    animSprite_fox->runAction(RepeatForever::create(animate_fox));
+    this->addChild(animSprite_fox, 1);
+    animFrames.clear();
+
+    return true;
+}
+
 bool HelloWorld::init() {
     if(!Scene::init()) {
         return false;
@@ -22,124 +53,126 @@ bool HelloWorld::init() {
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+    offset = cocos2d::Vec2(0, 0);
+    cellSize = cocos2d::Vec2(64, 64);
+    halfSize = cocos2d::Vec2(32, 32);
+    cellsCount = cocos2d::Vec2(4, 4);
+    speed = cocos2d::Vec2(1, 1);
+    currentCell = cocos2d::Vec2(0, 0);
+    gridRect = cocos2d::Rect(0, 0, cellSize.x * cellsCount.x, cellSize.y * cellsCount.y);
+    moving = false;
+    //pg.setWorldSize(N2Vector(cellsCount.x, cellsCount.y));
+    //pg.setDiagonalMovement(false);
+
     auto listener = EventListenerMouse::create(); 
     listener->onMouseMove = CC_CALLBACK_1(HelloWorld::onMouseMove, this);
     listener->onMouseDown = CC_CALLBACK_1(HelloWorld::onMouseDown, this);
     _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
 
-    auto closeItem = MenuItemImageExt::create("CloseNormal.png", "CloseSelected.png", "", CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0) {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+    auto cache = SpriteFrameCache::getInstance();
+    if (!cache) {
+        return false;
     }
-    else {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
+    cache->addSpriteFramesWithFile("anim/grass.plist");
+    auto sprite = Sprite::createWithSpriteFrame(cache->getSpriteFrameByName("gr0"));
+    sprite->setPosition(300, 300);
+    sprite->setScale(4.0);
+    sprite->getTexture()->setAliasTexParameters();
+    this->addChild(sprite);
 
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+    auto sprite2 = Sprite::createWithSpriteFrame(cache->getSpriteFrameByName("gr1"));
+    sprite2->setPosition(370, 300);
+    sprite2->setScale(4.0);
+    sprite2->getTexture()->setAliasTexParameters();
+    this->addChild(sprite2);
 
-    label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr) {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else {
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-          origin.y + visibleSize.height - label->getContentSize().height));
-        this->addChild(label, 1);
-    }
-
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr) {
-        problemLoading("'HelloWorld.png'");
-    }
-    else {
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-        this->addChild(sprite, 0);
-    }
-
-    SpriteFrameCache::getInstance()->addSpriteFramesWithFile("anim/out.plist");
-    auto spritecache = SpriteFrameCache::getInstance();
-    Vector<SpriteFrame*> animFrames;
-    animFrames.pushBack(spritecache->getSpriteFrameByName("pt0"));
-    animFrames.pushBack(spritecache->getSpriteFrameByName("pt1"));
-    animFrames.pushBack(spritecache->getSpriteFrameByName("pt2"));
-    animFrames.pushBack(spritecache->getSpriteFrameByName("pt3"));
-    animSprite = Sprite::createWithSpriteFrame(animFrames.front());
-    this->addChild(animSprite, 0);
-    animSprite->setPosition(100, 600);
-    animSprite->setName("anim");
-    animSprite->setVisible(false);
-    animation = Animation::createWithSpriteFrames(animFrames, 1.0f / 4);
-    animate = Animate::create(animation);
-    animSprite->runAction(RepeatForever::create(animate));
-
-    areasOffset = Vec2(32, 32);
-    halfSize = Size(32, 32);
-    fullSize = Size(64, 64);
-    cellsNumber[0] = cellsNumber[1] = 3;
-    overNum[0] = overNum[1] = -1;
-    clicked = -1;
-    over = -1;
-    available.push_back(0);
-    available.push_back(2);
-    available.push_back(4);
-
-    for (int i = 0; i < cellsNumber[0]; i++) {
-        for (int j = 0; j < cellsNumber[1]; j++) {
-            cocos2d::Sprite* sp = Sprite::create("quad64.png");
-            if (sp) {
-                this->addChild(sp, 0);
-                sp->setPosition(Vec2(areasOffset.x + i * fullSize.width, areasOffset.y + j * fullSize.height));
-                for (int k = 0; k < available.size(); k++) {
-                    if(available[k] == i* cellsNumber[0] + j) sp->setColor(Color3B(255, 128, 128));
-                }
-                cells.push_back(sp);
-            }
-        }
-    }
+    createAnimSpriteFromPlist("anim/out.plist", "selection", "pt", 4, 0.2);
+    this->getChildByName("selection")->setPosition(100, 600);
+    this->getChildByName("selection")->setScale(1.0);
+    this->getChildByName("selection")->setVisible(false);
+    createAnimSpriteFromPlist("anim/fox.plist", "anim_fox", "fox_pt", 4, 0.1);
+    this->getChildByName("anim_fox")->setPosition(700, 80);
+    this->getChildByName("anim_fox")->setScale(4.0);
+    this->getChildByName("anim_fox")->setVisible(true);
 
     return true;
 }
 
 void HelloWorld::update(float delta) {
+    /*
+    if (moving) {
+        if (path.empty()) {
+            moving = false;
+        }
+        else {
+            N2Vector n2v = path.front();
+            currentCell = cocos2d::vec2(n2v.x, n2v.y);
+            cocos2d::vec2 destination = cocos2d::vec2(offset.x + n2v.x * size.x + halfSize.x, offset.y + n2v.y * size.y + halfSize.y);
+            if (sprite.pos == destination) {
+                path.erase(path.begin());
+            }
+            else {
+                moveSprite(sprite, destination);
+            }
+        }
+    }
+    */
 }
 
 void HelloWorld::menuCloseCallback(Ref* pSender) {
     Director::getInstance()->end();
 }
 
-void HelloWorld::onMouseMove(cocos2d::Event* event) {
-    EventMouse* e = (EventMouse*)event;
-    Vec2 mc = Vec2(e->getCursorX(), e->getCursorY());
-    overNum[0] = mc.x / fullSize.width;
-    overNum[1] = mc.y / fullSize.height;
-    if (overNum[0] < 0 || overNum[1] < 0 || overNum[0] >= cellsNumber[0] || overNum[1] >= cellsNumber[1]) {
-        if (over != -1) animSprite->setVisible(false);
+cocos2d::Vec2 HelloWorld::getCellUnderMouse(cocos2d::Event* event) {
+    cocos2d::EventMouse* e = (EventMouse*)event;
+    cocos2d::Vec2 cell = cocos2d::Vec2(e->getCursorX(), e->getCursorY());
+    if (gridRect.containsPoint(cell)) {
+        cell = cocos2d::Vec2((int)(offset.x + cell.x / gridRect.size.width), (int)(offset.y + cell.y / gridRect.size.height));
+    }
+    else {
+        cell = cocos2d::Vec2(-1, -1);
+    }
+    return cell;
+}
+
+void HelloWorld::selectCell(cocos2d::Vec2 cell, std::string name) {
+    if (cell.x < 0 || cell.y < 0) {
+        this->getChildByName(name)->setVisible(false);
         return;
     }
-    over = overNum[0] * cellsNumber[0] + overNum[1];
-    if (clicked == over) return;
-    animSprite->setVisible(true);
-    animSprite->setPosition(cells[over]->getPosition());
+    this->getChildByName(name)->setPosition(getCoordsByCell(cell));
+    this->getChildByName(name)->setVisible(true);
+}
+
+cocos2d::Vec2 HelloWorld::getCoordsByCell(cocos2d::Vec2 cell) {
+    return cocos2d::Vec2(offset.x + cell.x * cellSize.x + halfSize.x, offset.y + cell.y * cellSize.y + halfSize.y);
+}
+
+cocos2d::Vec2 HelloWorld::sign(cocos2d::Vec2 vec) {
+    return cocos2d::Vec2((0 < vec.x) - (vec.x < 0), (0 < vec.y) - (vec.y < 0));
+}
+
+void HelloWorld::moveSprite(cocos2d::Sprite& sprite, cocos2d::Vec2 destination) {
+    cocos2d::Vec2 direction = sign(destination - sprite.getPosition());
+    cocos2d::Vec2 step = cocos2d::Vec2(speed.x * direction.x, speed.y * direction.y);
+    sprite.setPosition(sprite.getPosition() + step);
+}
+
+void HelloWorld::onMouseMove(cocos2d::Event* event) {
+    selectCell(getCellUnderMouse(event), "selection");
 }
 
 void HelloWorld::onMouseDown(Event* event) {
-    EventMouse* e = (EventMouse*)event;
-    Vec2 mc = Vec2(e->getCursorX(), e->getCursorY());
-    overNum[0] = mc.x / fullSize.width;
-    overNum[1] = mc.y / fullSize.height;
-    if (overNum[0] < 0 || overNum[1] < 0 || overNum[0] >= cellsNumber[0] || overNum[1] >= cellsNumber[1]) return;
-    int num = overNum[0] * cellsNumber[0] + overNum[1];
-    EventMouse::MouseButton btn = e->getMouseButton();
-    if (btn == EventMouse::MouseButton::BUTTON_LEFT) {
-        if(clicked != -1) cells[clicked]->setColor(Color3B(255, 255, 255));
-        cells[num]->setColor(Color3B(128, 128, 128));
-        clicked = num;
+    if (moving) {
+        return;
     }
+    cocos2d::Vec2 cell = getCellUnderMouse(event);
+    if (cell.x < 0 || cell.y < 0) {
+        return;
+    }
+    if (cell == currentCell) {
+        return;
+    }
+    //std::vector<N2Vector> path = pg.findPath(N2Vector(cell.x, cell.y), N2Vector(sprite->getPositionX(), sprite->getPositionY()));
+    //moving = true;
 }
--- cocosApp1/Classes/HelloWorldScene.h
+++ cocosApp1/Classes/HelloWorldScene.h
@@ -3,22 +3,25 @@
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::Scene
-{
+class HelloWorld : public cocos2d::Scene {
 private:
-    cocos2d::Label* label;
-    cocos2d::Vec2 areasOffset;
-    cocos2d::Size halfSize;
-    cocos2d::Size fullSize;
-    int cellsNumber[2];
-    int overNum[2];
-    std::vector<cocos2d::Sprite*> cells;
-    std::vector<int> available;
-    int clicked;
-    int over;
-    cocos2d::Sprite* animSprite;
-    cocos2d::Animation* animation;
-    cocos2d::Animate* animate;
+    cocos2d::Vec2 offset;
+    cocos2d::Rect gridRect;
+    cocos2d::Vec2 cellsCount;
+    cocos2d::Vec2 cellSize;
+    cocos2d::Vec2 halfSize;
+    cocos2d::Vec2 speed;
+    cocos2d::Vec2 currentCell;
+    bool moving;
+
+    cocos2d::Vec2 getCellUnderMouse(cocos2d::Event* event);
+    void selectCell(cocos2d::Vec2 cell, std::string name);
+    cocos2d::Vec2 getCoordsByCell(cocos2d::Vec2 cell);
+    cocos2d::Vec2 HelloWorld::sign(cocos2d::Vec2 vec);
+    void moveSprite(cocos2d::Sprite& sprite, cocos2d::Vec2 destination);
+
+    bool createAnimSpriteFromPlist(const std::string& fileName, const std::string& spriteName, const std::string& prefix, int count, float step);
+
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
