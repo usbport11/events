b9f03ce32091a5d6c966727aaf6c21d37309c72d
usbport11 <51235513+usbport11@users.noreply.github.com>
2025-01-28 21:31:51 +0300

    Add files via upload

---
+++ cocosApp1/Classes/AdventurerMenu.cpp
@@ -0,0 +1,144 @@
+#include "AdventurerMenu.h"
+#include "MainScene.h"
+#include <iostream>
+
+MAdventurerMenu::MAdventurerMenu() {
+	pMainScene = nullptr;
+}
+
+MAdventurerMenu::~MAdventurerMenu() {
+	numberAdventurer.clear();
+	adventurerSprite.clear();
+}
+
+void MAdventurerMenu::menuAdv0Callback(cocos2d::Ref* pSender) {
+	selectMenuItem(pSender);
+	//pMainScene->advClicked(numberAdventurer[0]);
+}
+void MAdventurerMenu::menuAdv1Callback(cocos2d::Ref* pSender) {
+	selectMenuItem(pSender);
+	//pMainScene->advClicked(numberAdventurer[1]);
+}
+void MAdventurerMenu::menuAdv2Callback(cocos2d::Ref* pSender) {
+	selectMenuItem(pSender);
+	//pMainScene->advClicked(numberAdventurer[2]);
+}
+void MAdventurerMenu::menuAdv3Callback(cocos2d::Ref* pSender) {
+	selectMenuItem(pSender);
+	//pMainScene->advClicked(numberAdventurer[3]);
+}
+void MAdventurerMenu::menuAdv4Callback(cocos2d::Ref* pSender) {
+	selectMenuItem(pSender);
+	//pMainScene->advClicked(numberAdventurer[4]);
+}
+void MAdventurerMenu::menuAdv5Callback(cocos2d::Ref* pSender) {
+	selectMenuItem(pSender);
+	//pMainScene->advClicked(numberAdventurer[5]);
+}
+
+bool MAdventurerMenu::create(MMainScene* _pMainScene, const std::string& pListFile, std::vector<std::string> adventurers) {
+    if (!_pMainScene) {
+        return false;
+    }
+    pMainScene = _pMainScene;
+	
+	cocos2d::SpriteFrameCache* cache = cocos2d::SpriteFrameCache::getInstance();
+    if (!cache) return false;
+    cache->addSpriteFramesWithFile(pListFile);
+	
+	std::map<std::string, std::string> advNameFrame = {
+		{"diver", "adv0"},
+		{"engineer", "adv1"},
+		{"explorer", "adv2"},
+		{"liaison", "adv3"},
+		{"navigator", "adv4"},
+		{"pilot", "adv5"}
+	};
+	
+	cocos2d::SpriteFrame* spriteFrame;
+	cocos2d::Sprite* sprite;
+	for(int i=0; i<adventurers.size(); i++) {
+		spriteFrame = cache->getSpriteFrameByName(advNameFrame[adventurers[i]]);
+		sprite = cocos2d::Sprite::createWithSpriteFrame(spriteFrame);
+		adventurerSprite[adventurers[i]] = sprite;
+	}
+	
+	//create items
+    std::map<int, cocos2d::ccMenuCallback> menuCallback = {
+		{0, CC_CALLBACK_1(MAdventurerMenu::menuAdv0Callback, this)},
+		{1, CC_CALLBACK_1(MAdventurerMenu::menuAdv1Callback, this)},
+		{2, CC_CALLBACK_1(MAdventurerMenu::menuAdv2Callback, this)},
+		{3, CC_CALLBACK_1(MAdventurerMenu::menuAdv3Callback, this)},
+		{4, CC_CALLBACK_1(MAdventurerMenu::menuAdv4Callback, this)},
+		{5, CC_CALLBACK_1(MAdventurerMenu::menuAdv5Callback, this)},
+	};
+	cocos2d::Vector<cocos2d::MenuItem*> menuItems;
+	cocos2d::Vec2 offset = cocos2d::Vec2(100, 100);
+	for(int i=0; i<adventurerSprite.size(); i++) {
+		cocos2d::MenuItemImage* menuItem = cocos2d::MenuItemImage::create("empty_card.png", "", "", menuCallback[i]);
+        if(!menuItem) {
+            return false;
+        }
+        menuItem->setPosition(offset.x + , offset.y + );//<--- need calculate
+		menuItem->setVisible(false);
+		menuItem->setEnabled(false);
+        menuItems.pushBack(menuItem);
+	}
+
+	//create menu
+    cocos2d::Menu* menu = cocos2d::Menu::createWithArray(menuItems);
+    if(!menu) {
+        return false;
+    }
+    menu->setPosition(cocos2d::Vec2::ZERO);
+    menu->setName("adventurerMenu");
+    pMainScene->addChild(menu, 1);
+    menuCallback.clear();
+
+    return true;
+}
+
+bool MAdventurerMenu::init(std::vector<std::string> activeAdventurers) {
+	numberAdventurer.clear();
+
+	cocos2d::Node* node = pMainScene->getChildByName("adventurerMenu");
+	if(!node) return false;
+    cocos2d::Vector<cocos2d::Node*> items = node->getChildren();
+	cocos2d::MenuItemImage* menuItem;
+    for (int i = 0; i < items.size(); i++) {
+		menuItem = (cocos2d::MenuItemImage*)items.at(i);
+		if(i < activeAdventurers.size()) {
+			menuItem->setVisible(true);
+			menuItem->setEnabled(true);
+			menuItem->setNormalImage(adventurerSprite[activeAdventurers[i]]);
+			menuItem->setSelectedImage(adventurerSprite[activeAdventurers[i]]);
+			numberAdventurer[i] = activeAdventurers[i];
+			if(i == 0) {
+				menuItem->setSelectedImage(adventurerSprite[activeAdventurers[i]]);
+			}
+		}
+		else {
+			menuItem->setVisible(false);
+			menuItem->setEnabled(false);
+		}
+	}
+	return true;
+}
+
+void MAdventurerMenu::unselectMenuAll() {
+    cocos2d::Node* node = pMainScene->getChildByName("adventurerMenu");
+    cocos2d::Vector<cocos2d::Node*> items = node->getChildren();
+	cocos2d::MenuItemImage* menuItem;
+    for (int i = 0; i < items.size(); i++) {
+		menuItem = (cocos2d::MenuItemImage*)items.at(i);
+		if(!menuItem->isEnabled()) break;
+		menuItem->setColor(cocos2d::Color3B(255, 255, 255));
+    }
+}
+
+void MAdventurerMenu::selectMenuItem(cocos2d::Ref* pSender) {
+    unselectMenuAll();
+	cocos2d::MenuItemImage* menuItem = (cocos2d::MenuItemImage*)pSender;
+    menuItem->setColor(cocos2d::Color3B(0, 255, 0));
+    menuItem->selected();
+}
---
+++ cocosApp1/Classes/AdventurerMenu.h
@@ -0,0 +1,28 @@
+#ifndef adventurerMenuH
+#define adventurerMenuH
+
+#include "cocos2d.h"
+
+class MMainScene;
+
+class MAdventurerMenu {
+private:
+	MMainScene* pMainScene;
+	std::map<int, std::string> numberAdventurer;
+	std::map<std::string, cocos2d::Sprite*> adventurerSprite; 
+	void menuAdv0Callback(cocos2d::Ref* pSender);
+	void menuAdv1Callback(cocos2d::Ref* pSender);
+	void menuAdv2Callback(cocos2d::Ref* pSender);
+	void menuAdv3Callback(cocos2d::Ref* pSender);
+	void menuAdv4Callback(cocos2d::Ref* pSender);
+	void menuAdv5Callback(cocos2d::Ref* pSender);
+	void unselectMenuAll();
+	void selectMenuItem(cocos2d::Ref* pSender);
+public:
+	MAdventurerMenu();
+	~MAdventurerMenu();
+	bool create(MMainScene* _pMainScene, const std::string& pListFile, std::vector<std::string> adventurers);
+	bool init(std::vector<std::string> activeAdventurers);
+};
+
+#endif
