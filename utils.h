0a9700fd8553aa6459a4223840a8f2e2d6f46ac7
usbport11 <poisk.ctulhu@gmail.com>
2025-04-01 19:16:43 +0300

    Revert "Add files via upload"
    This reverts commit 6ad53118c5c8f8d802d44455a5d6d8a9cb5b988f.

--- utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef utilsH
-#define utilsH
-
-#include "cocos2d.h"
-
-bool createAnimSpriteFromPlist(cocos2d::Scene* scene, const std::string& fileName, const std::string& spriteName, const std::string& prefix, int count, float step);
-
-#endif
